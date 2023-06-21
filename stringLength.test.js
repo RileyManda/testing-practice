ca19c19737db767171e831197c54a167b63cf428
RileyManda <rileymanda69@gmail.com>
2023-06-21 13:46:34 +0200

    Removed describe to fulfil the requirements of task 1 and task 2

--- stringLength.test.js
+++ stringLength.test.js
@@ -1,19 +1,17 @@
 const stringLength = require('./stringLength.js');
 
-describe('stringLength', () => {
-  test('returns the correct count of the string length', () => {
-    const testCases = [
-      { string: 'A', expectedLength: 1 }, // lenght of string is at least 1 character
-      { string: 'Helloworld', expectedLength: 10 }, // lenght of string is at least 10 characters
-    ];
+test('returns the correct count of the string length', () => {
+  const testCases = [
+    { string: 'A', expectedLength: 1 }, // lenght of string is at least 1 character
+    { string: 'Helloworld', expectedLength: 10 }, // lenght of string is at least 10 characters
+  ];
 
-    testCases.forEach(({ string, expectedLength }) => {
-      expect(() => {
-        stringLength(string);
-      }).not.toThrow(); // Ensure no error is thrown
+  testCases.forEach(({ string, expectedLength }) => {
+    expect(() => {
+      stringLength(string);
+    }).not.toThrow(); // Ensure no error is thrown
 
-      const result = stringLength(string);
-      expect(result).toBe(expectedLength);
-    });
+    const result = stringLength(string);
+    expect(result).toBe(expectedLength);
   });
 });
