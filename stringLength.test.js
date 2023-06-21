153acdb418300650c6fce138aca8171eeac1cff0
RileyManda <rileymanda69@gmail.com>
2023-06-21 13:16:30 +0200

    Implemented function error check on string lenght and update test case with this test case

--- stringLength.test.js
+++ stringLength.test.js
@@ -3,11 +3,15 @@ const stringLength = require('./stringLength.js');
 describe('stringLength', () => {
   test('returns the correct count of the string length', () => {
     const testCases = [
-      { string: 'A', expectedLength: 1 },
-      { string: 'Helloworld', expectedLength: 10 },
+      { string: 'A', expectedLength: 1 }, // lenght of string is at least 1 character
+      { string: 'Helloworld', expectedLength: 10 }, // lenght of string is at least 10 characters
     ];
 
     testCases.forEach(({ string, expectedLength }) => {
+      expect(() => {
+        stringLength(string);
+      }).not.toThrow(); // Ensure no error is thrown
+
       const result = stringLength(string);
       expect(result).toBe(expectedLength);
     });
