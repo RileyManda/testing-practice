48fa2781ca3229476a1ae07906d78ae509927d9f
RileyManda <rileymanda69@gmail.com>
2023-06-21 15:13:16 +0200

    Implemented test for multiply

--- calculator.js
+++ calculator.js
@@ -3,13 +3,13 @@ class Calculator {
 
   subtract = (a, b) => a - b;
 
+  multiply = (a, b) => a * b;
+
   divide = (a, b) => {
     if (b === 0) {
       throw new Error('Cannot divide by zero.');
     }
     return a / b;
   };
-
-  multiply = (a, b) => a * b;
 }
 module.exports = Calculator;
--- calculator.test.js
+++ calculator.test.js
@@ -14,4 +14,21 @@ describe('Calculator', () => {
       expect(result).toBe(5);
     });
   });
+  // Test cases for the multiply method
+  describe('multiply', () => {
+    test('returns the product of two positive numbers', () => {
+      const result = calculator.multiply(4, 3);
+      expect(result).toBe(12);
+    });
+
+    test('returns the product of a positive and a negative number', () => {
+      const result = calculator.multiply(5, -3);
+      expect(result).toBe(-15);
+    });
+
+    test('returns the product of two negative numbers', () => {
+      const result = calculator.multiply(-7, -4);
+      expect(result).toBe(28);
+    });
+  });
 });
