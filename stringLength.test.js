1985f959bfbd9ca82414bd71c3c3f9abdd110f6a
RileyManda <rileymanda69@gmail.com>
2023-06-21 12:57:08 +0200

    Implemented string returns at least 1 char,no more than 10

--- stringLength.test.js
+++ stringLength.test.js
@@ -3,10 +3,8 @@ const stringLength = require('./stringLength.js');
 describe('stringLength', () => {
   test('returns the correct count of the string length', () => {
     const testCases = [
-      { string: '', expectedLength: 0 },
-      { string: 'Hello, world!', expectedLength: 13 },
-      { string: 'OpenAI', expectedLength: 6 },
-      { string: '12345', expectedLength: 5 },
+      { string: 'A', expectedLength: 1 },
+      { string: 'Helloworld', expectedLength: 10 },
     ];
 
     testCases.forEach(({ string, expectedLength }) => {
